64ca62be85acc75e691edc861ae4067c624bfbad
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-05 23:38:42 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treate all JS code as newer version
+
+//alert("hello")//we are using nodejs ,not browser
+//javascript ka jo engine hai vo browser k andar chupa hua hota hai.
+
+console.log(3+3)//code readibility
+console.log("hitesh")
+
+let name="mohit"
+let age=18
+let isloggedIn =false
+
+//number=> 2 to power 53
+//bigint(used for making big websites such as stocks ,facebook, reddit etc)
+//string  = ""
+//boolean => true/false
+//null => (representation of empty value)standalone value(type = object)
+//undefined => 
+//symbol => unique
+
+
+//object
+console.log(typeof "mohit")
+console.log(typeof age)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
