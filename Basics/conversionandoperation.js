f3d9e9bfec3b3266e55a263e7706a2ef7be5b612
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-06 00:30:01 +0000

    conversion

---
+++ Basics/conversionandoperation.js
@@ -0,0 +1,28 @@
+let score=null
+
+console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber)
+//console.log(typeof score)
+//console.log(valueInNumber)
+
+//"333" =>33
+//"33abc" => NaN
+//TRUE =>1; False=>0
+
+let isloggedIn = ""
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn)
+console.log(typeof isloggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+//1=>true; 0=> false
+//""=>false
+//"mohit" => true
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
