edee0b0c91e3440c90c152ffba3e7a2b6591c4d2
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-07 00:46:31 +0000

    datatypessummary

---
+++ Basics/datatypessummary.js
@@ -0,0 +1,17 @@
+const score=100
+const scorevalue=100.3
+const isloggedIn=false
+const outsidetemp=null
+let useremail=undefined
+const id=Symbol('123')
+const aid=Symbol('123')
+// console.log(id==aid);
+// console.log(typeof score);
+// console.log(typeof scorevalue)
+// console.log(typeof isloggedIn);
+// console.log(typeof outsidetemp)
+// console.log(typeof useremail)
+// console.log(typeof id)
+// console.log(typeof aid)
+
+console.log(id===aid)
