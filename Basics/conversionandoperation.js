4e752c9f2b7b085624159f58af85574e94e8018b
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-07 00:44:59 +0000

    Fullconversion

--- Basics/conversionandoperation.js
+++ Basics/conversionandoperation.js
@@ -1,6 +1,6 @@
 let score=null
 
-console.log(typeof score)
+//console.log(typeof score)
 //console.log(typeof (score))
 
 let valueInNumber =Number(score)
@@ -14,9 +14,9 @@ let valueInNumber =Number(score)
 
 let isloggedIn = ""
 let booleanIsLoggedIn = Boolean(isloggedIn);
-console.log(booleanIsLoggedIn)
-console.log(typeof isloggedIn)
-console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+//console.log(typeof isloggedIn)
+//console.log(typeof booleanIsLoggedIn)
 
 //1=>true; 0=> false
 //""=>false
@@ -24,5 +24,56 @@ console.log(typeof booleanIsLoggedIn)
 
 let someNumber=33
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+//console.log(stringNumber);
+//console.log(typeof(stringNumber))
+
+//--------------------OPPERATIONS----------------
+
+let value=3
+
+let negvalue=-value
+//console.log(negvalue)
+
+//console.log(2+2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1="hello"
+str2=" JS"
+let str3=str1+str2
+//console.log(str3)
+
+//console.log("1 "+ 2)
+//console.log(1 +" 2")
+//console.log("1"+2+2)
+//console.log(1+2+"2");
+
+//console.log(true);
+//console.log(+true);
+//console.log(+"");
+
+let n1,n2,n3
+n1=n2=n3=2+2
+
+//console.table([n1,n2,n3]);
+
+let gamecounter=100
+let gc=11
+let a=++gamecounter
+let b=gc++
+console.log(gamecounter);
+console.log(a);
+
+console.log(b);
+console.log(gc)
+
+
+
+
+
+
+
+
+
