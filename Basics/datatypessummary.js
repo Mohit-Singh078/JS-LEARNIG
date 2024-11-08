51760a303ef190dd8ddd760daea1ee2303cce023
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-08 00:58:52 +0000

    datatypesupdated

--- Basics/datatypessummary.js
+++ Basics/datatypessummary.js
@@ -1,3 +1,4 @@
+/*
 const score=100
 const scorevalue=100.3
 const isloggedIn=false
@@ -14,4 +15,41 @@ const aid=Symbol('123')
 // console.log(typeof id)
 // console.log(typeof aid)
 
-console.log(id===aid)
+//console.log(id===aid)
+const bigNumber=55678578n
+//console.log(typeof bigNumber)
+
+const heroes = ["shaktiman","naaga","sushant"];
+let myobj={
+    name:"hitesh",
+    age:22,
+}
+console.log(typeof heroes)
+console.log(typeof myobj)
+
+const myfunction=function(){
+    console.log("hello world");
+}
+console.log(typeof myfunction)
+*/
+
+//___________---------------------------memory
+//2 types (stack(primitive), heap(non primitive))
+
+
+let myyoutube="mohitgotcom"
+let anothername=myyoutube
+anothername="javascript"
+console.log(myyoutube)
+console.log(anothername)
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let usertwo=userOne
+usertwo.email="mohit@google.com"
+console.log(userOne)
+console.log(usertwo)
+
+
