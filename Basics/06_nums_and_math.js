ef7980e2ff18f54364b6b4f0db3a99f898c79a0d
Mohit-Singh078 <mohitpratapsingh029@gmail.com>
2024-11-09 00:20:00 +0000

    number nad math

---
+++ Basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score=400
+//console.log(score)
+
+const balance=new Number("mohit")
+//console.log(balance);
+//console.log(typeof balance);
+
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(8));
+
+const otherno=123.8966
+//console.log(otherno.toPrecision(6));
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//______________________MATHS_______________
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(625))
+console.log(Math.min(7,5,8,3,5))
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
